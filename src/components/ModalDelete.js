3868ba8a7e2d6cf8568ad548222c8a6fbec93e18
Mhafizd21 <maulanahafizd21@gmail.com>
2021-07-31 11:49:48 +0700

    refactor class component to function component

--- src/components/ModalDelete.js
+++ src/components/ModalDelete.js
@@ -2,38 +2,36 @@ import React from 'react';
 import Button from './Button';
 import '../styles/Modal.css';
 
-class ModalEdit extends React.Component {
-  render() {
-    const { modalState, close, data, del } = this.props;
-    const delById = id => del(id)
+const ModalEdit = props => {
+  const { modalState, close, data, del } = props;
+  const delById = id => del(id)
 
-    if (!modalState) {
-      return null;
-    }
+  if (!modalState) {
+    return null;
+  }
 
-    return (
-      <div className="modal__container">
-        <div className="modal">
-          <div className="modal__header">
-            <h3 className="modal__title"> Delete Task</h3>
-          </div>
-          <div className="modal__body">
-            <p> 
-              Delete
-              <span>{" "}"{data.title}"{" "} </span>
-              from list ?
-            </p>
-          </div>
-          <div className="modal__footer">
-            <div className="btn__group">
-              <Button variant="danger" text="Cancel" action={() => close('delete')}/>
-              <Button variant="success" text="Delete" action={() => delById(data.id)}/>
-            </div>
+  return (
+    <div className="modal__container">
+      <div className="modal">
+        <div className="modal__header">
+          <h3 className="modal__title"> Delete Task</h3>
+        </div>
+        <div className="modal__body">
+          <p> 
+            Delete
+            <span>{" "}"{data.title}"{" "} </span>
+            from list ?
+          </p>
+        </div>
+        <div className="modal__footer">
+          <div className="btn__group">
+            <Button variant="danger" text="Cancel" action={() => close('delete')}/>
+            <Button variant="success" text="Delete" action={() => delById(data.id)}/>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default ModalEdit;
